7ba619f4efcdfb906bb562835b9ec91fc57ea864
Lin Ye <linyesh@amazon.com>
2023-01-07 14:33:19 +0800

    add complicate user content resolving

--- kinesis_to_msk/app.py
+++ kinesis_to_msk/app.py
@@ -10,4 +10,19 @@ def lambda_handler(event, context):
                              value_serializer=lambda m: json.dumps(m).encode())
     for record in event['Records']:
         payload = base64.b64decode(record["kinesis"]["data"])
-        producer.send(config.TOPIC, json.loads(payload))
+
+        user_data = json.loads(payload)
+
+        is_custom_topic = False
+        if 'attributes' in user_data:
+            if 'customer_context' in user_data['attributes']:
+                if 'topic_name' in user_data['attributes']['customer_context']:
+                    try:
+                        print('topic is [' + user_data['attributes']['customer_context']['topic_name'] + ']')
+                        producer.send(user_data['attributes']['customer_context']['topic_name'], user_data)
+                        is_custom_topic = True
+                    except Exception as e:
+                        print(e)
+
+        if not is_custom_topic:
+            print('no topic found')
